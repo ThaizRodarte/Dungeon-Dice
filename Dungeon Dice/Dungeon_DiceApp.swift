fde493ba58b8378953f7d251b99eb2907d5b47de
ThaizRodarte <thaizrodarte08@gmail.com>
2026-02-05 12:44:05 -0800

    Initial Commit

---
+++ Dungeon Dice/Dungeon_DiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dungeon_DiceApp.swift
+//  Dungeon Dice
+//
+//  Created by RODARTE, THAIZ on 2/5/26.
+//
+
+import SwiftUI
+
+@main
+struct Dungeon_DiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
